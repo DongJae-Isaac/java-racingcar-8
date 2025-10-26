33270b92dee56901caab04740573e529322e7cd5
DongJae Kim <ehdwo1227@naver.com>
2025-10-26 16:38:19 +0900

    docs(Cars): createCars 메소드에 JavaDoc 주석 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -12,6 +12,11 @@ public class Cars {
         this.cars =  createCars(name);
     }
 
+    /**
+     * 쉼표를 기준으로 분리된 자동차를 Car타입의 리스트로 변환
+     * @param names 쉼표를 기준으로 분리된 자동차 이름 배열
+     * @return Car클래스 타입을 가지는 리스트
+     */
     public List<Car> createCars(String[] names) {
         List<Car> carsList = new ArrayList<>();
         for(String name : names) {
