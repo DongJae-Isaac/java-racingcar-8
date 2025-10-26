f2103cc60b6815b45938433b3c2ce0bae8e1e714
DongJae Kim <ehdwo1227@naver.com>
2025-10-26 16:30:18 +0900

    refactor(Application): 기능별로 클래스 분리 후 Application에 통합

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        String input = inputHandler.getCarInput();
+        int round = inputHandler.getRoundsInput();
+
+        Cars cars = new Cars(input);
+
+        Racing racing = new Racing(cars, round);
+
+        WinnerHandler winnerHandler = new WinnerHandler(cars);
+        winnerHandler.findWinner();
+        winnerHandler.getWinner();
+
     }
 }
